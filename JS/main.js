e475d5398ab04a5e6f09809bb2ae2212cb444d9c
Axel <axel.montlahuc@stjoavignon.com>
2023-10-28 12:10:04 +0200

    Added a CSS Grid for the GitHub data

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,15 @@ const SearchButton = document.querySelector('.search-button');
 const SearchInput = document.querySelector('.search-input');
 const SearchBar = document.querySelector('.search-container');
 const UserInfos = document.querySelector('.user-infos-container');
+const GitHubAvatar = document.querySelector('.github-avatar');
+const GitHubName = document.querySelector('.github-name');
+const GitHubBio = document.querySelector('.github-bio');
+const GitHubEmail = document.querySelector('.github-email');
+const GitHubLocation = document.querySelector('.github-location');
+const GitHubCompany = document.querySelector('.github-company');
+const GitHubCreationDate = document.querySelector('.github-creation-date');
+const GitHubFollowers = document.querySelector('.github-followers');
+const GitHubFollowing = document.querySelector('.github-following');
 
 async function fetchGitHubUserData(username) {
     const response = await fetch(`https://api.github.com/users/${username}`);
@@ -17,10 +26,13 @@ function handleSearch() {
     }
 
     UserInfos.style.display = 'flex';
-    SearchBar.style.top = '12.5%'
+    SearchBar.style.top = '12.5%';
 
     let GitHubData = fetchGitHubUserData(Username)
-    console.log(GitHubData)
+    GitHubAvatar.style.backgroundImage = `url(https://avatars.githubusercontent.com/${Username})`;
+    GitHubName.innerHTML = GitHubData.data.name;
+    console.log(GitHubData.data.name);
+    console.log(GitHubData);
 }
 
 SearchButton.addEventListener('click', handleSearch);
